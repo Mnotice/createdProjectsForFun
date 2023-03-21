37c49c52e279e55c22e0190d4037a0836b410762
Mnotice <62525324+Mnotice@users.noreply.github.com>
2023-03-21 14:26:51 +0200

    Create Program.cs
    a guess the word game.
    user needs to guess the word, and they have 4 chances of guessing correctly.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string secretWord = "giraffe";
+string guess = "";
+int guessLimit = 4;
+
+while(guess != secretWord)
+{
+    Console.Write("Enter guess: ");
+    guess = Console.ReadLine();
+}
+
+Console.Write("You Win!");
+
+Console.ReadLine();
